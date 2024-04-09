2d2fa284933faf54e622df2473e157ca887f26bc
Your Name <you@example.com>
2024-04-09 11:18:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function loading(){
+    alert("Loading weather report")
+}
+function choosedegree(element) {
+    alert("You picked " + element.value);
+}
+function afterr(){
+    document.getElementById('policy')
+    .innerHTML=" ";
+}
